d4cdab420305444f2910cf69be2f5f941e1e795b
pradeepnnv <pradeepnnv@gmail.com>
2023-06-19 23:17:58 -0500

    hello-world 1
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	// got := Hello("Chris")
+	// want := "Hello, Chris"
+	// if got != want {
+	// 	t.Errorf("got %q want %q", got, want)
+	// }
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+	t.Run("say 'Hello, World' when empty string response is provided", func(t *testing.T) {
+		got := Hello("World")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
