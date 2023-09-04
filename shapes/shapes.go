19911f81af34cc70826e6d9b8fc7b63469e47b05
pradeepnnv <pradeepnnv@gmail.com>
2023-09-04 16:55:07 -0500

    shapesv2
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -30,3 +30,12 @@ func (c Circle) Area() float64 {
 type Shape interface {
 	Area() float64
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
