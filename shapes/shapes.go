1ce31561e2cde6c8f2df3868d055847e638f036c
pradeepnnv <pradeepnnv@gmail.com>
2023-09-04 16:46:05 -0500

    shapesv1
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

---
+++ shapes/shapes.go
@@ -0,0 +1,32 @@
+package shapes
+
+import "math"
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
+}
