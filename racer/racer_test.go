75e76f4cbd0c4e8668991df4932b820b913eac9c
pradeepnnv <pradeepnnv@gmail.com>
2025-02-11 19:29:30 -0600

    update racer test

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -1,11 +1,26 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "https://facebook.com"
-	fastURL := "https://quii.dev"
-
+	// slowURL := "https://facebook.com"
+	// fastURL := "https://quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer slowServer.Close()
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer fastServer.Close()
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 	if got != want {
